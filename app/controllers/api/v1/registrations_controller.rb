16e94051e76e6cb32f5dcf37e4effc89ee715d12
Harumasa Iino <haru10masa18@gmail.com>
2024-05-29 23:29:32 +0900

    registrations

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,19 +3,19 @@ module Api
     class RegistrationsController < BaseController
       def create
         @user = User.new(user_params)
-          if @user.save
-            json_string = UserSerializer.new(@user).serialized_json
-            render json: json_string
-          else
-            render_request(nil, @user.errors.full_messages)
-          end
+        if @user.save
+          json_string = UserSerializer.new(@user).serialized_json
+          render json: json_string
+        else
+          render_request(nil, @user.errors.full_messages)
         end
+      end
 
-        private
+      private
 
-        def user_params
-          params.require(:user).permit(:name, :email, :password)
-        end
+      def user_params
+        params.require(:user).permit(:name, :email, :password)
+      end
     end
   end
 end
