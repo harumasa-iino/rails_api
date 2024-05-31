fcc0e9be9178f15eaa2d32d1b8f09a8ab4118831
Harumasa Iino <haru10masa18@gmail.com>
2024-05-31 23:46:23 +0900

    api_key

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,10 +3,23 @@
 module Api
   module V1
     class BaseController < ApplicationController
+      include ActionController::HttpAuthentication::Token::ControllerMethods
       include ExceptionHandler
       def set_token(user)
         api_key = user.grant_api_key
         response.headers['AccessToken'] = api_key.access_token
+      end
+
+      private
+
+      def current_user
+        @_current_user
+      end
+
+      def authenticate
+        authenticate_or_request_with_http_token do |token, options|
+          @_current_user ||= ApiKey.valid_expire.find_by(access_token: token)&.user
+        end
       end
     end
   end
